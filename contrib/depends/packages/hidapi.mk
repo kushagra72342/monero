0dcab02afe2295f8553f991fa0e6644fb394ef42
kushagra72342 <rajsinghkushagra@gmail.com>
2025-02-07 20:41:06 +0530

    .

---
+++ contrib/depends/packages/hidapi.mk
@@ -0,0 +1,27 @@
+package=hidapi
+$(package)_version=0.14.0
+$(package)_download_path=https://github.com/libusb/hidapi/archive/refs/tags
+$(package)_file_name=$(package)-$($(package)_version).tar.gz
+$(package)_sha256_hash=a5714234abe6e1f53647dd8cba7d69f65f71c558b7896ed218864ffcf405bcbd
+$(package)_linux_dependencies=libusb
+
+define $(package)_set_vars
+  $(package)_config_opts+=-DBUILD_SHARED_LIBS=OFF -DHIDAPI_WITH_HIDRAW=OFF
+endef
+
+# Remove blobs
+define $(package)_preprocess_cmds
+  rm -rf documentation testgui windows/test/data m4
+endef
+
+define $(package)_config_cmds
+  $($(package)_cmake) .
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
